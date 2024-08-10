a9c4ea70c62a882542ed07ae2269f93c3fa4588f
Yeseul Lee <yesl3715@naver.com>
2024-08-10 22:49:36 +0900

    Init setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,74 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    // 함수 반환 타입을 명시하도록 강제
+    "@typescript-eslint/explicit-function-return-type": "error",
+    // 네이밍의 일관성을 위한 규칙을 설정
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: [
+          "variableLike",
+          "classProperty",
+          "objectLiteralProperty",
+          "typeProperty",
+          "classMethod",
+          "objectLiteralMethod",
+          "typeMethod",
+          "accessor",
+        ],
+        format: ["camelCase"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: ["variable"],
+        types: ["function"],
+        format: ["camelCase", "PascalCase"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: ["variable"],
+        modifiers: ["global"],
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+      },
+      {
+        selector: [
+          "classProperty",
+          "objectLiteralProperty",
+          "typeProperty",
+          "classMethod",
+          "objectLiteralMethod",
+          "typeMethod",
+          "accessor",
+          "enumMember",
+        ],
+        format: null,
+        modifiers: ["requiresQuotes"],
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
