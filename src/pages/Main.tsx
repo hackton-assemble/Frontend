a8f20b0ca7f446d360c0d6a58af2524fb4cd7f02
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-08-11 22:47:22 +0900

    feat: 페이지 디렉토리 추가

---
+++ src/pages/Main.tsx
@@ -0,0 +1,11 @@
+import DefaultButton from '../components/DefaultButton';
+
+const Main = () => {
+  return (
+    <div>
+      <DefaultButton size={'S'} style={'primary'} />
+    </div>
+  );
+};
+
+export default Main;
