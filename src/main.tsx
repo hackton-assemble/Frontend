3d2e7712f6cce3bb1229f9cfda038d771d921a56
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-08-11 22:48:56 +0900

    refactor: globalstyle, router 적용 및 리팩토링

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme.ts';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './pages/Main.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <RouterProvider router={router} />
+  </ThemeProvider>,
 );
