125a2bfaeb28d8534512b46f7c74c1a0c57fc714
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-08-12 17:07:59 +0900

    feat: RecoilRoot 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme.ts';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.tsx';
+import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={theme}>
-    <RouterProvider router={router} />
-  </ThemeProvider>,
+  <RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </RecoilRoot>,
 );
