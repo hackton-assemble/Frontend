9f751dc917353d3f9a1cc08d37cfcb36b1f6c497
mimpie <yesl3715@naver.com>
2024-08-13 12:58:54 +0900

    Init Setting

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme.ts';
 import { RouterProvider } from 'react-router-dom';
-import router from './router.tsx';
 import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
