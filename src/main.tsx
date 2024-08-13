1832b9f2ced91a6a6163103437a7abaf1607179e
myeolinmalchi <rkd2274@pusan.ac.kr>
2024-08-13 16:46:40 +0900

    feat: Add router in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme.ts';
-import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import Main from './pages/Main.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <RecoilRoot>
